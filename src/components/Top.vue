a7881f73cbbe075f4a1c6469a90b03c3ac368c45
huma17ax <genbot.think17Virt@gmail.com>
2025-03-03 02:10:18 +0900

    [feat] Delete Sample Page

---
+++ src/components/Top.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// TopPageコンポーネント
+defineOptions({
+  name: 'TopPage',
+})
+</script>
+
+<template>
+  <div class="top-container">
+    <h1>トップページ</h1>
+  </div>
+</template>
+
+<style scoped>
+.top-container {
+  padding: 2rem;
+  text-align: center;
+}
+</style>
